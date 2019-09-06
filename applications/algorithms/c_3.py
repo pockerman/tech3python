517aac419d042952906653ca92d6a76ed4dc2109
pockerman <upperframeworks@gmail.com>
2019-09-06 09:31:05 +0300

    Update C3 algorithm

--- applications/algorithms/c_3.py
+++ applications/algorithms/c_3.py
@@ -3,6 +3,8 @@ Python uses the C3 algorithm to figure out the class
 inheritance. Code from the book: Pro Python 3, 3rd Edition
 """
 
+import itertools
+
 def c_3(cls, *mro_lists):
     """
     Implement the C3 algorithm
@@ -14,4 +16,43 @@ def c_3(cls, *mro_lists):
     # the new MRO with the class itself
     mro = [cls, ]
 
+    while True:
+
+        candidate_found = False
+        for mro_list in mro_lists:
+
+            if not len(mro_list):
+                continue
+
+            if candidate_found:
+                if candidate in mro:
+                    mro_list.pop(0)
+                continue
+
+            candidate = mro_list[0] # the potential candiadate for MRO
+
+            if candidate in itertools.chain(*(x[1:] for x in mro_lists)):
+                continue
+            else:
+
+                # candidate is valid so promote to MRO
+                mro.append(candidate)
+                mro_list.pop(0)
+                candidate_found = True
+
+        if not sum(len(mro_list) for mro_list in mro_lists):
+            # there are no MROs to cycle through
+            break
+
+        if not candidate_found:
+            # No valid candidate was available, so we have to bail out.
+            break
+            raise TypeError("Inconsistent MRO")
+
     return mro
+
+
+if __name__ == '__main__':
+
+    # should print ['C', 'B', 'A', 'object']
+    print(c_3('C', ['B', 'A', 'object'], ['A', 'object'], ['B', 'A']))
