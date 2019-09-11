60c43cc1f0989f343953779ff696419ecca2dd8e
David Hilbert <a.giavaras@gmail.com>
2019-09-11 12:43:24 +0300

    First commit for statistics/example_5

--- README.md
+++ README.md
@@ -17,9 +17,20 @@ This is my Python playground...
 
 - <a href="https://github.com/pockerman/tech3python/blob/master/applications/statistics/example_1.py">Example 1</a>
 - <a href="https://github.com/pockerman/tech3python/blob/master/applications/statistics/example_2.py">Example 2</a>
-- <a href="https://github.com/pockerman/tech3python/blob/master/applications/statistics/example_3.py">Example 3</a>
-- <a href="https://github.com/pockerman/tech3python/blob/master/applications/statistics/example_4.py">Example 4</a>
+- <a href="https://github.com/pockerman/tech3python/blob/master/applications/statistics/example_3.py">Example 3</a>: Vanilla bootstrap for the mean
+- <a href="https://github.com/pockerman/tech3python/blob/master/applications/statistics/example_4.py">Example 4</a>: Vanilla bootstrap for skewness
+- <a href="https://github.com/pockerman/tech3python/blob/master/applications/statistics/example_5.py">Example 5</a>: 1D Linear Regression 
+
 
 ### Estimation
+
+- <a href="#">Example 1</a>: Kalman Filter
+- <a href="#">Example 2</a>: Extended Kalman Filter
+- <a href="#">Example 3</a>: Unscented Kalman Filter
+- <a href="#">Example 4</a>: Particle Filter
+- <a href="#">Example 5</a>: Histogram Filter
+
+
+### Control
 
 ### Machine Learning
---
+++ applications/statistics/example_5.py
@@ -0,0 +1,43 @@
+"""
+
+Category: Statistics
+ID: Example 5
+Description: Simple Linear Regression with scikit-learn
+Taken From: https://realpython.com/linear-regression-in-python/
+Dependencies NumPy, scikit-learn
+"""
+
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+
+def main(*args, **kwargs):
+
+    x = np.array([5, 15, 25, 35, 45, 55]).reshape((-1, 1))
+    y = np.array([5, 20, 14, 32, 22, 38])
+
+    # plot the data
+
+
+    """
+    Create the model:
+    
+    
+    - fit_intercept is a Boolean (True by default) that decides whether to calculate the intercept 𝑏₀ (True) or consider it equal to zero (False).
+    - normalize is a Boolean (False by default) that decides whether to normalize the input variables (True) or not (False).
+    - copy_X is a Boolean (True by default) that decides whether to copy (True) or overwrite the input variables (False).
+    - n_jobs is an integer or None (default) and represents the number of jobs used in parallel computation. None usually means one job and -1 to use all processors.
+
+    """
+    model = LinearRegression(fit_intercept=True, normalize=False, copy_X=True, n_jobs=None)
+    model.fit(x ,y )
+
+    print('intercept:', model.intercept_)
+    print('slope:', model.coef_)
+    print('coefficient of determination:', model.score(x, y))
+
+
+if __name__ == '__name__':
+
+    print("Running Example statistics/example_5")
+    main()
