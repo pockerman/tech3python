9c4d60b4fd94264d250c79858818595cf00b95eb
pockerman <a.giavaras@gmail.com>
2019-09-23 11:10:53 +0300

    #5 Add class for PreorderTraversal

--- algorithms/preorder_tree_traversal.py
+++ algorithms/preorder_tree_traversal.py
@@ -2,8 +2,11 @@
 Preorder tree traversal algorithm
 """
 
-def preorder_traversal(root, predicate):
 
+def preorder_tree_traversal(root, predicate):
+    """
+    Preorder traversal algorithm
+    """
     if root is None:
         raise ValueError("Root node is None")
 
@@ -24,8 +27,40 @@ def preorder_traversal(root, predicate):
             idx = c
             break
 
-        child = preorder_traversal(child, predicate)
+        root, child, _ = preorder_tree_traversal(child, predicate)
+
+        if predicate(child):
+            idx = c
+            break
 
     return root, child, idx
+
+
+class PreorderTraversal:
+
+    #def __init__(self):
+    #    pass
+
+    @staticmethod
+    def insert(root, value, predicate):
+
+        """
+        Insert the given value in the tree represented by the root.
+        Insertion position is determined by the given predicate
+        """
+        root, child, idx = preorder_tree_traversal(root, predicate)
+
+        if child is None:
+            root.create_and_set_child(idx, value)
+            return True
+
+        child.set_data(data=value)
+        child.set_parent(parent=root)
+        root.set_child(idx, item=child)
+        return True
+
+    def __call__(self, *args, **kwargs):
+        self.insert(root=args[0], value=args[1], predicate=args[2])
+
 
 
