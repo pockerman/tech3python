77bc588828270a2ff660c6f4e1cde1d576821e71
pockerman <a.giavaras@gmail.com>
2019-09-23 11:10:10 +0300

    #5 Update TreeNode class and push method

--- adt/binary_tree.py
+++ adt/binary_tree.py
@@ -1,16 +1,22 @@
 from .tree_base import TreeBase
 from .tree_base import TreeNode
+from predicates.basic_predicates import IsNonePredicate
 
 
 class BinaryTree(TreeBase):
+
+    """
+    Models a binary tree
+    """
 
     def __init__(self, insert_method):
         TreeBase.__init__(self, insert_method=insert_method)
 
     def push(self, value):
+
         """
-        Adds a new value in the ADT. Concrete classes specify
-        where the push occurs
+        Adds a new value in the Binary tree. Insertion is done according to the
+        insertion method specified at construction time
         """
 
         if self.get_root() is None:
@@ -19,7 +25,8 @@ class BinaryTree(TreeBase):
             self._make_root(node)
             return
 
-        sucess = self._insert_method.insert(root=self._root, value=value)
+        predicate = IsNonePredicate()
+        sucess = self._get_insert_method().insert(root=self._root, value=value, predicate=predicate)
 
         if sucess:
             self._size += 1
--- adt/tree_base.py
+++ adt/tree_base.py
@@ -18,10 +18,32 @@ class TreeNode:
         if self._parent is not None:
             self._level = self._parent.get_level() + 1
 
+    def set_data(self, data):
+
+        """
+        Set the data stored by this node
+        """
+        self._data = data
+
+    def get_data(self):
+
+        """
+        Returns the data stored by the node
+        """
+        return self._data
+
     def set_child(self, idx, item):
+
+        """
+        Set the idx-th child of this node
+        """
         self._children[idx] = item
 
     def get_child(self, idx):
+
+        """
+        Get the idx-th child
+        """
         return self._children[idx]
 
     def set_children(self, children):
@@ -38,16 +60,41 @@ class TreeNode:
         return len(self._children)
 
     def get_level(self):
+
+        """
+        Returns the level of the node
+        """
         return self._level
 
     def has_parent(self):
+
         """
         Returns True if the parent of this node is not None
         """
         return self._parent is not None
 
+    def set_parent(self, parent):
+
+        """
+        Set the parent node
+        """
+        self._parent = parent
+
+        # by setting the parent implicitly we set the level also
+        if self._parent is not None:
+            self._level = self._parent.get_level() + 1
+
+    def create_and_set_child(self, idx, item):
+        node = TreeNode(data=item, parent=self)
+        node.set_children(children=[None for i in range(self.n_children())])
+        self.set_child(idx, node)
+
 
 class TreeBase(ADTBase):
+
+    """
+    Base class for trees
+    """
 
     def __init__(self, insert_method):
         super(TreeBase, self).__init__()
@@ -56,27 +103,38 @@ class TreeBase(ADTBase):
         self._insert_method = insert_method
 
     def __len__(self):
+
         """
-        Returns the number of elements present in the ADT
+        Returns the number of elements present in the tree
         """
         return self._size
 
     def empty(self):
+
         """
-        Returns true if the ADT is empty
+        Returns true if the tree is empty
         """
         return self._size == 0
 
     def get_root(self):
+
         """
         Returns the root node
-        :return:
         """
         return self._root
 
     def _make_root(self, node):
+
+        """
+        Assign the given node as the root of the tree
+        """
+
         self._root = node
         self._size += 1
 
     def _get_insert_method(self):
+
+        """
+        Returns the insertion method used by the tree
+        """
         return self._insert_method
