04aee9472552eda0d33e787ccda74496253f7a77
David Hilbert <a.giavaras@gmail.com>
2019-10-13 17:18:28 +0300

    #60 Add classification of handwritten digits

--- applications/machine_learning/example_6.py
+++ applications/machine_learning/example_6.py
@@ -43,7 +43,6 @@ def main():
     plt.clim(-0.5, 9.5)
     plt.show()
 
-
     Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, random_state=0)
 
     # create the model
@@ -57,6 +56,15 @@ def main():
     plt.xlabel('predicted value')
     plt.ylabel('true value')
     plt.show()
+
+    fig, axes = plt.subplots(10, 10, figsize=(8, 8),
+                            subplot_kw={'xticks':[], 'yticks':[]}, gridspec_kw=dict(hspace=0.1, wspace=0.1))
+
+    for i, ax in enumerate(axes.flat):
+        ax.imshow(digits.images[i], cmap='binary', interpolation='nearest')
+        ax.text(0.05, 0.05, str(y_model[i]), transform=ax.transAxes, color='green' if (ytest[i] == y_model[i]) else 'red')
+    plt.show()
+
 
 
 if __name__ == '__main__':
